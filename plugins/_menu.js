a26cb57ecfb2a78cc6b52d33696692b06395ada3
Jayden-official <pancafe198@gmail.com>
2024-08-30 10:47:30 +0300

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -93,7 +93,7 @@ bot.addCommand(
       }
     })
     const [date, time] = getDate()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ MAADIVA ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${PREFIX}
 ┃❃│ User : ${message.pushName}
